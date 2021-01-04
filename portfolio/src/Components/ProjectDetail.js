c878602740d99696389d694a7e919b0dd298afb2
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-03 19:54:34 -0500

    readjusted portfolio cards, thinking of changing layout of landing page on smaller devices

--- portfolio/src/Components/ProjectDetail.js
+++ portfolio/src/Components/ProjectDetail.js
@@ -6,9 +6,10 @@ import { FaGithub } from 'react-icons/fa';
 const ProjectDetail = ({project })=> {
     return(
         <div className='pdContainer'>
-            <div className='pdHeaderRow'>
-                <div><h1 className='pdTitle'>{project.title}</h1></div>
-                <div className='pdLinkRow'>
+            <div className='pdImgAndDetails'>
+                <img className='pdImg' alt={project.title} src={require(`../assets/images/projectImages/${project.image}`)} />
+                <div className='pdDetails'>
+                    <h2>{project.title}</h2>
                     <a className='pdLink' 
                        target='_blank'
                        rel='noopener noreferrer' 
@@ -17,15 +18,8 @@ const ProjectDetail = ({project })=> {
                        target='_blank'
                        rel='noopener noreferrer' 
                        href={project.githubLink}><FaGithub /> View Repo</a>
-                </div>
-            </div>
-            <div className='pdImgAndDetails'>
-                <img className='pdImg' alt={project.title} src={require(`../assets/images/projectImages/${project.image}`)} />
-                <div className='pdDetails'>
-                    <h3>Tech:</h3>
-                    <div><p>{project.tech}</p></div>
-                    <h3>Description:</h3>
-                    <div><p>{project.body}</p></div>
+                    <div className='pdBody'><p>{project.body}</p></div>
+                    <div className='pdTech'><p>{project.tech}</p></div>
                 </div>
             </div>
         </div>
