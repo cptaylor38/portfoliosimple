5529ebb2112df36a226b80aa8aaa9329b7256837
Corbin Taylor <cptaylor38@yahoo.com>
2021-01-04 18:40:53 -0500

    debugging

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -11,7 +11,7 @@ const Contact = ()=> {
                 <ExternalLinks />
             </header>
             <div id='contactFormContainer'>
-            <form method="post" data-netlify-honeypot="bot-field" data-netlify="true" name="contact" action='/contact' onSubmit={()=> console.log('submitted')}></form>
+            <form method="post" data-netlify-honeypot="bot-field" data-netlify="true" name="contact"></form>
                 <label for='email'>Email:</label>
                 <input className='contactInput' type="email" name="email" placeholder='Jsmith@gmail.com:' />
                 <label for='name'>Name:</label>
@@ -19,7 +19,7 @@ const Contact = ()=> {
                 <label for='message'>Message:</label>
                 <textarea className='contactInput' type="text" name="message" rows={5} placeholder="I'd like to reach out about..." />
                 <input type="hidden" name="form-name" value="contact" />
-                <button id='contactButton' type="submit">Send</button>
+                <button id='contactButton'>Send</button>
             </div>
         </div>
     )
