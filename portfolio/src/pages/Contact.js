d285a1151547d06482f4ebd6a619757927dd0370
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-06 23:14:17 -0500

    fixed blank form submission and added recapctha, may change redirect after form submit at a later date

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ExternalLinks from '../Components/ExternalLinks';
-
 import '../style/Contact.css';
 
 const Contact = ()=> {
+    const [disabled, setDisabled] = useState(true);
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const changeHandler = (e)=> {
+        setFormData({...formData, [e.target.name] : e.target.value});
+    }
+
+    useEffect(()=> {
+        const {name, email, message} = formData;
+        if(name !== '' && email !== '' && message !== '') setDisabled(false);
+        else setDisabled(true);
+    }, [formData])
+    
     return (
         <div id='contact'>
             <header id='contactHeader'>
                 <h3>Interested in chatting? Contact me via the form, download my resume with the button below, shoot me an email, or reach me on LinkedIn or Github.</h3>
                 <ExternalLinks />
             </header>
-            <form name="contact" id='contactForm' method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="">
+            <form name="contact" id='contactForm' method="post" data-netlify="true" data-netlify-honeypot="bot-field" data-netlify-recaptcha="true" action="">
                 <label htmlFor='email'>Email:</label>
-                <input className='contactInput' type="email" name="email" placeholder='Jsmith@gmail.com:' />
+                <input onChange={changeHandler} className='contactInput' type="email" name="email" placeholder='Jsmith@gmail.com:' value={formData.email}/>
                 <label htmlFor='name'>Name:</label>
-                <input className='contactInput' type="text" name="name" placeholder='John Smith' />
+                <input onChange={changeHandler} className='contactInput' type="text" name="name" placeholder='John Smith' value={formData.name}/>
                 <label htmlFor='message'>Message:</label>
-                <textarea className='contactInput' type="text" name="message" rows="5" placeholder="I'd like to reach out about..." />
+                <textarea onChange={changeHandler} className='contactInput' type="text" name="message" rows="5" placeholder="I'd like to reach out about..." value={formData.message}/>
                 <input type="hidden" name="form-name" value="contact" />
-                <button id='contactButton'>Send</button>
+                <button id={disabled ? 'contactButtonDisabled' : 'contactButton'} disabled={disabled}>Send</button>
             </form>
         </div>
     )
