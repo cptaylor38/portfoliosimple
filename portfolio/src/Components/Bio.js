d8144e4e8c64e78a91e440c1dd9e31b82598c42e
Corbin Taylor <cptaylor38@yahoo.com>
2021-03-08 11:41:28 -0500

    removed null reference to faq

--- portfolio/src/Components/Bio.js
+++ portfolio/src/Components/Bio.js
@@ -22,7 +22,6 @@ const Bio = ({setShowBio}) => {
                     <p>In 2015 I graduated from GTCC with an Associates in Simulation and Game design. At the time I was much more interested in 3D Modeling and Environment design but knew that I wanted to explore programming and software development on a deeper level. </p>
                     <p>In 2019 I graduated from a six month Full Stack coding bootcamp at UNCC that led me to my current journey. It's a brand new medium, an expansive, rapidly changing, and highly impactful skillset that has allowed me to express my creativity in a whole new medium. As someone that's worked primarily in Retail and Healthcare, I've found myself at the mercy of the world and the way technology has continued to shape it. Now, I have the foundation for a skillset that allows me to grow with it and be a part of that change. </p>
                     <p>In my free time I'm often practicing front end design, learning, building a passion project, gaming, or exploring a newfound passion for health and fitness. I'm an avid animal lover, supporter of equality and diversity, and a self-proclaimed geek.</p>
-                    <p>If you're interested in learning more, please feel free to contact me and be sure to check the frequently asked questions section.</p>
                 </section>
             </div>
         </div>
